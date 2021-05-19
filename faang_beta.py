d9395d8781e8e2a6b9d652c4f41075a6aec24f15
Brody <45079557+brodyu@users.noreply.github.com>
2021-05-19 11:06:30 -1000

    done

--- faang_beta.py
+++ faang_beta.py
@@ -1,42 +1,47 @@
 import yfinance as yf
 from scipy import stats
 
-# class for calculating faang stock's beta
+# Class for calculating faang stock's beta
 class FaangBeta:
-    # constructor 
+    # Initialize ticker, start_date, and end_date values
     def __init__(self, ticker, start_date, end_date):
         self.ticker = ticker
         self.start_date = start_date
         self.end_date = end_date
 
-    def beta(self):
+    def get_historical(self):
+        # Take in ticker and index
         symbols = [self.ticker, '^GSPC']
         # API call to pull historical pricing data
         data = yf.download(symbols, self.start_date, self.end_date)['Adj Close']
-        # check for any null values and exit program if they exist
+        # Check for any null values and exit program if they exist
         if data.isnull().values.any() == True:
             print("Null values exist, exiting program")
             exit()
-        # gather percent change for the stock and index and eliminate first NA row
+        # Return dataframe
+        return data
+
+    def calculate_beta(self):
+        # Call yfinance API to pull data
+        data = self.get_historical()
+        # Gather percent change for the stock and index and eliminate first NA row
         x = data[data.columns[0]].pct_change()[1:]
         y = data[data.columns[1]].pct_change()[1:]
-        # find the line of best fit that minimizes the sum of the square errors
+        # Find the line of best fit that minimizes the sum of the square errors
         slope, intercept, r, p, std_err = stats.linregress(y, x)
-        # return the beta coefficient which is the slope of the line of best fit
+        # Return the beta coefficient which is the slope of the line of best fit
         return slope
         
 
-    
+# Main method    
 if __name__ == "__main__":
-    # FAANG Beta Coefficients
-    fb = FaangBeta("FB", '2016-01-01', '2021-01-01').beta()
-    aapl = FaangBeta("AAPL", '2016-01-01', '2021-01-01').beta()
-    amzn = FaangBeta("AMZN", '2016-01-01', '2021-01-01').beta()
-    nflx = FaangBeta("NFLX", '2016-01-01', '2021-01-01').beta()
-    googl = FaangBeta("GOOGL", '2016-01-01', '2021-01-01').beta()
+    # Pull FAANG Beta Coefficients
+    # start_date and end_date must follow 'YYYY-MM-DD' format
+    symbols = ["FB", "AAPL", "AMZN", "NFLX", "GOOG"]
+    for symbol in symbols:
+        stock = FaangBeta(symbol, '2016-05-19', '2021-05-19')
+        print(symbol, "beta :", stock.calculate_beta())
 
-    print('FB Beta: ', fb)
-    print('AAPL Beta: ', aapl)
-    print('AMZN Beta: ', amzn)
-    print('NFLX Beta: ', nflx)
-    print('GOOGL Beta: ', googl)
+    # Other use case: 
+    # fb = FaangBeta("FB", '2016-01-01', '2021-01-01').calculate_beta()
+    # print('FB Beta: ', fb)
